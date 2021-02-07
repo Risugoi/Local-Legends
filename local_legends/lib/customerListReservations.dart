afa4561c48e427275225a9d002c4b74005c3d43c
Mark de los Reyes <42885769+Risugoi@users.noreply.github.com>
2021-02-07 23:22:50 +0800

    database update in list of reservations customer screen

--- local_legends/lib/customerListReservations.dart
+++ local_legends/lib/customerListReservations.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:local_legends/styles/styles.dart';
 import 'package:local_legends/customerHome.dart';
+import 'package:path/path.dart' as p;
+import 'package:sqflite/sqflite.dart';
+import 'package:local_legends/database/customerdb.dart';
 
 class listOfReserve extends StatefulWidget {
   final String email;
@@ -13,9 +16,20 @@ class listOfReserve extends StatefulWidget {
 
 class _listOfReserve extends State<listOfReserve> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  List _reserveList = [];
+  int _n;
+  @override
+  void initState() {
+    super.initState();
+    info();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _n = _reserveList.length;
+    print(_reserveList);
+    print(_n);
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: new Scaffold(
@@ -34,23 +48,60 @@ class _listOfReserve extends State<listOfReserve> {
             ),
             body: ListView.builder(
               padding: const EdgeInsets.all(10),
-              itemCount: 10,
+              itemCount: _n,
               itemBuilder: (context, index) {
                 return ListTile(
                   contentPadding:
                       EdgeInsets.only(left: 10, right: 10, bottom: 20, top: 10),
                   leading: Image.asset('assets/images/logo.png'),
-                  title: Text('Resto Name'),
+                  title: Text(_reserveList[index].values.elementAt(0)),
                   subtitle: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text('City'),
-                      Text('Date'),
-                      Text('Time'),
+                      Text(_reserveList[index]
+                          .values
+                          .elementAt(3)
+                          .toString()
+                          .substring(0, 11)),
+                      Text(_reserveList[index].values.elementAt(4)),
                     ],
+                  ),
+                  trailing: RaisedButton(
+                    onPressed: null,
+                    child: Text('Cancel'),
                   ),
                 );
               },
             )));
+  }
+
+  void info() async {
+    try {
+      List reserveDetails = await _getReservationInfo();
+      setState(() {
+        _reserveList = reserveDetails;
+      });
+    } catch (e) {
+      print('error');
+    }
+  }
+
+  _getReservationInfo() async {
+    String path = p.join(await getDatabasesPath(), customerdb.dbName);
+    var db = await openDatabase(path);
+    List<Map> numRows = await db.rawQuery(
+        "SELECT COUNT (${customerdb.customerEmail}) from ${customerdb.table2}");
+    int getNum = numRows[0].values.elementAt(0);
+    List<Map> results = await db.rawQuery(
+        "SELECT COUNT(*) FROM ${customerdb.table2} WHERE ${customerdb.customerEmail}='${widget.email}'");
+    if (getNum == 0) {
+    } else {
+      int userCount = results[0].values.elementAt(0);
+      if (userCount > 0) {
+        List<Map> reservationList = await db.rawQuery(
+            "SELECT * FROM ${customerdb.table2} WHERE ${customerdb.customerEmail} = '${widget.email}'");
+        return reservationList.toList();
+      }
+    }
   }
 }
